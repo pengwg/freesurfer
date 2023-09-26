202d51cf018d3911451b8567b9feb2cc2c7deb6c
Peng Wang <pengwg@gmail.com>
2023-09-26 16:43:42 -0400

    Use system CMake modules for Qt5, OpenCV, X11 and VTK

--- cmake/FindANN.cmake
+++ cmake/FindANN.cmake
@@ -8,6 +8,6 @@ if(NOT ANN_DIR)
   endif()
 endif()
 
-find_path(ANN_INCLUDE_DIR HINTS ${ANN_DIR} NAMES ANN PATH_SUFFIXES include)
-find_library(ANN_LIBRARIES HINTS ${ANN_DIR} NAMES libANN.a PATH_SUFFIXES lib)
+find_path(ANN_INCLUDE_DIR NAMES ANN PATH_SUFFIXES include)
+find_library(ANN_LIBRARIES libann.a PATH_SUFFIXES lib)
 find_package_handle_standard_args(ANN DEFAULT_MSG ANN_INCLUDE_DIR ANN_LIBRARIES)
